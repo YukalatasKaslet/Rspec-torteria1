87e2644dc925154de35c9072ccc181781db6ebc4
codeamac01 <codeamac01@gmail.com>
2016-10-03 10:14:46 -0500

    pruebas 1,2,3,4,7,14 listas

--- lib/torta.rb
+++ lib/torta.rb
@@ -3,30 +3,35 @@ class Torta
 
   def initialize(type)
     @type = type
-    # @time_baked = 0
+    @time_baked = 0
   end
 
   def bake!(time)
-    # @time_baked += time
-    # self
+    @time_baked += time
+    self
   end
 
   def status
-    # status = @time_baked.to_f / bake_time
+    status = @time_baked.to_f / bake_time
 
-    # case
-    # when status < 0.5
-    #   'crudo'
-    # when status < 0.7
-    #   'casi listo'
-    # when status <= 1.0
-    #   'listo'
-    # when status > 1.0
-    #   'quemado'
-    # end
+    case
+    when status < 0.5
+      'crudo'
+    when status < 0.7
+      'casi listo'
+    when status <= 1.0
+      'listo'
+    when status > 1.0
+      'quemado'
+    end
   end
 
   def bake_time
-    # { "jamon" => 3, "queso" => 8, "milanesa" => 10 }[@type]
+    { "jamon" => 3, "queso" => 8, "milanesa" => 10 }[@type]
   end
 end
+
+# t = Torta.new("milanesa")
+# # p t.bake!(-2)
+# # p t.status
+# p t.bake_time
--- spec/torta_spec.rb
+++ spec/torta_spec.rb
@@ -5,9 +5,11 @@ describe Torta do
   let(:type)  { "milanesa" }
   let(:torta) { Torta.new(type) }
 
+  #Test1
   subject { torta }
   it { is_expected.to respond_to(:type) }
 
+  #Test 2 y 3
   describe "#initialize" do
     context "with valid input" do
       it "creates a new Torta of the specified type" do
@@ -22,20 +24,30 @@ describe Torta do
     end
   end
 
+  #Test 4
   describe "#type" do
-    it "returns the type of the torta"
+    it "returns the type of the torta" do
+      #expect(torta).to respond_to(:type)#.with(0).argument
+      expect(torta.type).to eq(type)
+    end
   end
 
+  #Test 5, 6, 7
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument"# do
+      #expect(torta).to respond_to(:bake!).with(2).argument
+    #end
 
-    it "returns the torta object"
+    it "returns the torta object"# do 
+      #expect(torta.bake!(1)).to eq(torta.class)
+    #end
 
     it "changes the status of the torta when given enough time" do
       expect { torta.bake!(10) }.to change(torta, :status)
     end
   end
 
+  #Test 8,9,10,11,12,13
   describe "#status" do
     it "returns the torta's current status"
 
@@ -60,7 +72,10 @@ describe Torta do
     end
   end
 
+  #Test 14
   describe "#bake_time" do
-    it "returns bake time of the specified type"
+    it "returns bake time of the specified type" do
+      expect(torta.bake_time).to eq(10)
+    end
   end
 end
